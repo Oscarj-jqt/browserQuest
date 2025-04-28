ac443be012eaffd42deb0eb2144cae8e540794f9
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-04-28 14:06:02 +0200

    config

--- server/js/main.js
+++ server/js/main.js
@@ -35,17 +35,7 @@ function main(config) {
 
     console.log("Starting BrowserQuest game server...");
 
-    server.on('request', (req, res) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-        if (req.method === 'OPTIONS') {
-          res.writeHead(204);
-          res.end();
-          return;
-        }
-    }
-)
+    
     server.onConnect(function(connection) {
         let world;
         
